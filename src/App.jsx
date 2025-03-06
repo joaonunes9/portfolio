aeb1ca6c6c648d56f80b50e1aee8d7b9e2f4205f
joaonunes9 <jp.nunes100@gmail.com>
2025-03-06 20:54:04 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <nav>Menu here</nav>
+      <main>Content here</main>
+      <footer>Footer here</footer>
+    </>
+  );
+}
